79b694faf92314be69917b8502a48fe6cbe47ed7
Daniel Calvin <arockne2003@yahoo.com>
2021-11-17 12:38:46 -0700

    commit tree 735ce46b4ca7d12e0a3a69f17c4dc2962db7e44f

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,17 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+    if (params[:name])
+      name = params[:name].downcase
+      students = students.filter do |s| 
+        s.first_name.downcase.include?(name) || s.last_name.downcase.include?(name)
+      end
+    end
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
 end
